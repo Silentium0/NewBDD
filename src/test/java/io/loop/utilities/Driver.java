6707759be08b57a8feabf80b64cba31adb8980ab
sergii <davygorasergii@gmail.com>
2025-03-02 08:22:00 -0500

    push

--- src/test/java/io/loop/utilities/Driver.java
+++ src/test/java/io/loop/utilities/Driver.java
@@ -1,76 +1,188 @@
 package io.loop.utilities;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import io.loop.utilities.ConfigurationReader;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.safari.SafariDriver;
 
-import java.time.Duration;
+import java.net.URL;
 
 public class Driver {
+    static String browser;
 
     private Driver() {
     }
 
-    /*
-    making driver instance private
-    static - run before everything else and also use in static method
-     */
+    private static WebDriver driver;
+    private static ChromeOptions chromeOptions;
+    private static FirefoxOptions firefoxOptions;
 
-    //private static WebDriver driver;
-    private static InheritableThreadLocal<WebDriver> drivePool = new InheritableThreadLocal<>();
+    private static DesiredCapabilities desiredCapabilities;
 
-
-    /*
-    reusable method that will return the same driver instance every time called
-     */
-
-    /**
-     * singleton pattern
-     *
-     * @return driver
-     */
     public static WebDriver getDriver() {
-        if (drivePool.get() == null) {
-            String browserType = ConfigurationReader.getProperty("browser");
-            switch (browserType.toLowerCase()) {
+        if (driver == null) {
+            if (System.getProperty("BROWSER") == null) {
+                browser = ConfigurationReader.getProperty("browser");
+            } else {
+                browser = System.getProperty("BROWSER");
+            }
+            System.out.println("Browser: " + browser);
+            switch (browser) {
+                case "remote-chrome":
+                    try {
+                        // assign your grid server address
+                        String gridAddress = "44.204.78.229";
+                        URL url = new URL("http://" + gridAddress + ":4444/wd/hub");
+                        desiredCapabilities = new DesiredCapabilities();
+                        desiredCapabilities.setBrowserName("chrome");
+                        driver = new RemoteWebDriver(url, desiredCapabilities);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    break;
+
+                case "remote-firefox":
+                    try {
+                        // assign your grid server address
+                        String gridAddress = "44.204.78.229";
+                        URL url = new URL("http://" + gridAddress + ":4444/wd/hub");
+                        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+                        desiredCapabilities.setBrowserName("firefox");
+                        driver = new RemoteWebDriver(url, desiredCapabilities);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    break;
+
                 case "chrome":
                     WebDriverManager.chromedriver().setup();
-                    drivePool.set(new ChromeDriver());
-                    drivePool.get().manage().window().maximize();
-                    drivePool.get().manage().timeouts().implicitlyWait(Duration.ofSeconds(Integer.valueOf(ConfigurationReader.getProperty("timeout"))));
+                    driver = new ChromeDriver();
                     break;
+
+                case "chrome-headless":
+//                    WebDriverManager.chromedriver().setup();
+//                    driver = new ChromeDriver(new ChromeOptions().setHeadless(true));
+                    ChromeOptions options = new ChromeOptions();
+                    options.addArguments("--headless"); // Enable headless mode
+                    //options.addArguments("start-maximized"); // maximize
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver(options);
+                    break;
+
                 case "firefox":
                     WebDriverManager.firefoxdriver().setup();
-                    drivePool.set(new FirefoxDriver());
-                    drivePool.get().manage().window().maximize();
-                    drivePool.get().manage().timeouts().implicitlyWait(Duration.ofSeconds(Integer.valueOf(ConfigurationReader.getProperty("timeout"))));
+                    driver = new FirefoxDriver();
                     break;
-                case "headless":
-                    ChromeOptions options = new ChromeOptions();
-                    options.addArguments("--headless");
-                    options.addArguments("--start-maximized");
+
+                case "firefox-headless":
+//                    WebDriverManager.firefoxdriver().setup();
+//                    driver = new FirefoxDriver(new FirefoxOptions().setHeadless(true));
+                    FirefoxOptions options2 = new FirefoxOptions();
+                    options2.addArguments("--headless"); // Enable headless mode
+                    //options.addArguments("start-maximized"); // maximize
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver(options2);
+                    break;
+
+                case "ie":
+                    if (System.getProperty("os.name").toLowerCase().contains("mac")) {
+                        throw new WebDriverException("Your operating system does not support the requested browser");
+                    }
+                    WebDriverManager.iedriver().setup();
+                    driver = new InternetExplorerDriver();
+                    break;
+
+                case "edge":
+                    if (System.getProperty("os.name").toLowerCase().contains("mac")) {
+                        throw new WebDriverException("Your operating system does not support the requested browser");
+                    }
+                    WebDriverManager.edgedriver().setup();
+                    driver = new EdgeDriver();
+                    break;
+
+                case "safari":
+                    if (System.getProperty("os.name").toLowerCase().contains("windows")) {
+                        throw new WebDriverException("Your operating system does not support the requested browser");
+                    }
+                    WebDriverManager.getInstance(SafariDriver.class).setup();
+                    driver = new SafariDriver();
+                    break;
+
+
+                /**
+                 * These added because of EC@2 Jenkins on Linux was not running the ones above because of graphical issues.
+                 */
+                case "chrome-linux":
                     WebDriverManager.chromedriver().setup();
-                    drivePool.set(new ChromeDriver(options));
-                    drivePool.get().manage().window().maximize();
-                    drivePool.get().manage().timeouts().implicitlyWait(Duration.ofSeconds(Integer.valueOf(ConfigurationReader.getProperty("timeout"))));
+                    chromeOptions = new ChromeOptions();
+                    chromeOptions.addArguments("--headless");
+                    chromeOptions.addArguments("--no-sandbox");
+                    chromeOptions.addArguments("--disable-dev-shm-usage");
+                    driver = new ChromeDriver(chromeOptions);
                     break;
 
-            }
+                case "remote-chrome-linux":
+                    try {
+                        // assign your grid server address
+                        String gridAddress = "44.204.78.229";
+                        URL url = new URL("http://" + gridAddress + ":4444/wd/hub");
+                        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+                        chromeOptions = new ChromeOptions();
+                        chromeOptions.addArguments("--headless");
+                        chromeOptions.addArguments("--no-sandbox");
+                        chromeOptions.addArguments("--disable-dev-shm-usage");
+                        desiredCapabilities.merge(chromeOptions);
+                        driver = new RemoteWebDriver(url, desiredCapabilities);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    break;
 
+                case "firefox-linux":
+                    WebDriverManager.firefoxdriver().setup();
+                    firefoxOptions = new FirefoxOptions();
+                    firefoxOptions.addArguments("--headless");
+                    firefoxOptions.addArguments("--disable-gpu");
+                    firefoxOptions.addArguments("--no-sandbox");
+                    driver = new FirefoxDriver(firefoxOptions);
+                    break;
+
+                case "remote-firefox-linux":
+                    try {
+                        // assign your grid server address
+                        String gridAddress = "44.204.78.229";
+                        URL url = new URL("http://" + gridAddress + ":4444/wd/hub");
+                        desiredCapabilities = new DesiredCapabilities();
+                        desiredCapabilities.setBrowserName("firefox");
+                        FirefoxOptions firefoxOptions = new FirefoxOptions();
+                        firefoxOptions.addArguments("--headless");
+                        firefoxOptions.addArguments("--disable-gpu");
+                        firefoxOptions.addArguments("--no-sandbox");
+                        desiredCapabilities.merge(firefoxOptions);
+                        driver = new RemoteWebDriver(url, desiredCapabilities);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    break;
+            }
         }
-        return drivePool.get();
+
+        return driver;
     }
 
-    /**
-     * closing driver
-     */
     public static void closeDriver() {
-        if (drivePool.get() != null) {
-            drivePool.get().quit();
-            drivePool.remove();
+        if (driver != null) {
+            driver.quit();
+            driver = null;
         }
     }
 }
-
